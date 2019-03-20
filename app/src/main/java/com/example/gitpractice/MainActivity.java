5f11ea1324cee19dfd3b87820978e9b78e2ca6b3
jngmui <wjdal208@gmail.com>
2019-03-20 15:17:08 +0900

    Initial Commit

--- app/src/main/java/com/example/gitpractice/MainActivity.java
+++ app/src/main/java/com/example/gitpractice/MainActivity.java
@@ -6,8 +6,16 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
